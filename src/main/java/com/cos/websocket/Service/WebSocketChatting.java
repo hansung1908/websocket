8f2fdf8216f5ba5d6a05ef7a7abb57922f364ea2
hansung1908 <hansung1908@naver.com>
2023-04-05 23:49:18 +0900

    websocket 구현 완료

--- src/main/java/com/cos/websocket/Service/WebSocketChatting.java
+++ src/main/java/com/cos/websocket/Service/WebSocketChatting.java
@@ -11,7 +11,7 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
-@ServerEndpoint(value = "/chat")
+@ServerEndpoint(value = "/chatt")
 @Service
 public class WebSocketChatting {
 
